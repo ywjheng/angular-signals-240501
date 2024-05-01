82aeb39b23ae1dab715b1cc90fc25abd9c207d33
Ivy <ywjheng@gmail.com>
2024-05-01 14:47:49 +0800

    m5 - subscribe to the returned observable

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit, inject } from '@angular/core';
 
 import { NgIf, NgFor, NgClass } from '@angular/common';
 import { Product } from '../product';
 import { ProductDetailComponent } from '../product-detail/product-detail.component';
+import { ProductService } from '../product.service';
+import { Subscription, tap } from 'rxjs';
 
 @Component({
     selector: 'pm-product-list',
@@ -10,13 +12,31 @@ import { ProductDetailComponent } from '../product-detail/product-detail.compone
     standalone: true,
   imports: [NgIf, NgFor, NgClass, ProductDetailComponent]
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit, OnDestroy {
   // Just enough here for the template to compile
   pageTitle = 'Products';
   errorMessage = '';
+  sub!: Subscription;
+
+  private productService = inject(ProductService);
 
   // Products
   products: Product[] = [];
+
+  ngOnInit(): void {
+    this.productService.getProducts()
+    .pipe(
+      tap(() => console.log(`In component pipeline`))
+    ).subscribe(products => {
+      this.products = products
+      console.log(this.products);
+      
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
 
   // Selected product id to highlight the entry
   selectedProductId: number = 0;
