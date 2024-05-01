1ceefe26706e004df5aeb9aaa4523750673dad44
Ivy <ywjheng@gmail.com>
2024-05-01 15:26:44 +0800

    m6 - handling http errors in service

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -1,23 +1,31 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
-import { Observable, map, tap } from 'rxjs';
+import { Observable, catchError, map, of, tap, throwError } from 'rxjs';
 import { Product } from './product';
+import { ProductData } from './product-data';
+import { HttpErrorService } from '../utilities/http-error.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
-  private productsUrl = 'api/products';
+  private productsUrl = 'api/productss';
 
   // ctor-based DI
   // constructor(private http: HttpClient) {}
   // NG14+ function injection
   private http = inject(HttpClient);
+  private errorService = inject(HttpErrorService);
 
   getProducts(): Observable<Product[]> {
     return this.http.get<Product[]>(this.productsUrl)
     .pipe(
-      tap(() => console.log(`In http.get pipeline`))
+      tap(() => console.log(`In http.get pipeline`)),
+      catchError((error => this.handleError(error)))
+      // catchError(error => {
+      //   console.error(error);
+      //   return of(ProductData.products);
+      // })
     );
   }
 
@@ -27,6 +35,12 @@ export class ProductService {
     .pipe(
       tap(() => console.log(`In http.get by id pipeline`)),
     );
+  }
+
+  private handleError(error: HttpErrorResponse): Observable<never> {
+    const formattedMessage = this.errorService.formatError(error);
+    return throwError(() => formattedMessage);
+    // throw formattedMessage;
   }
 
 }
