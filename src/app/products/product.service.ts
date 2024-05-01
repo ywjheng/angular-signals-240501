2b40d076276082d5ba3b01b7230bea1930e64008
Ivy <ywjheng@gmail.com>
2024-05-01 15:01:17 +0800

    m5 - retrieve one product item by id

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
-import { Observable, tap } from 'rxjs';
+import { Observable, map, tap } from 'rxjs';
 import { Product } from './product';
 
 @Injectable({
@@ -18,6 +18,14 @@ export class ProductService {
     return this.http.get<Product[]>(this.productsUrl)
     .pipe(
       tap(() => console.log(`In http.get pipeline`))
+    );
+  }
+
+  getProduct(id: number): Observable<Product> {
+    const productUrl = `${this.productsUrl}/${id}`;
+    return this.http.get<Product>(productUrl)
+    .pipe(
+      tap(() => console.log(`In http.get by id pipeline`)),
     );
   }
 
