c5834624c91c280a6ec206d658e1ede9b1f2a845
Ivy <ywjheng@gmail.com>
2024-05-01 14:31:20 +0800

    m5 - retrieve items in product service

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -1,10 +1,24 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { Observable, tap } from 'rxjs';
+import { Product } from './product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
-  // Just enough here for the code to compile
   private productsUrl = 'api/products';
+
+  // ctor-based DI
+  // constructor(private http: HttpClient) {}
+  // NG14+ function injection
+  private http = inject(HttpClient);
+
+  getProducts(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.productsUrl)
+    .pipe(
+      tap(() => console.log(`In http.get pipeline`))
+    );
+  }
 
 }
