0253021f3e365bdbe5767a4f8b541265c97cb57b
Ivy <ywjheng@gmail.com>
2024-06-03 16:10:06 +0800

    m11 - use Singal update to update cart items

--- src/app/cart/cart.service.ts
+++ src/app/cart/cart.service.ts
@@ -1,9 +1,16 @@
-import { Injectable, signal } from "@angular/core";
+import { Injectable, effect, signal } from "@angular/core";
 import { CartItem } from "./cart";
+import { Product } from "../products/product";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
   cartItems = signal<CartItem[]>([]);
+  eLength = effect(() => console.log(`Cart array length: ${this.cartItems().length}`));
+
+  addToCart(product: Product): void {
+    // this.cartItems().push({ product, quantity: 1});
+    this.cartItems.update(items => [...items, { product, quantity: 1}]);
+  }
 }
--- src/app/products/product-detail/product-detail.component.ts
+++ src/app/products/product-detail/product-detail.component.ts
@@ -3,6 +3,7 @@ import { NgIf, NgFor, CurrencyPipe, AsyncPipe } from '@angular/common';
 import { Product } from '../product';
 import { EMPTY, Subscription, catchError } from 'rxjs';
 import { ProductService } from '../product.service';
+import { CartService } from 'src/app/cart/cart.service';
 
 @Component({
     selector: 'pm-product-detail',
@@ -16,6 +17,7 @@ export class ProductDetailComponent {
   sub!: Subscription;
 
   private productService = inject(ProductService);
+  private cartService = inject(CartService);
 
   // Product to display
   product$ = this.productService.product$
@@ -31,5 +33,6 @@ export class ProductDetailComponent {
   pageTitle = `ProductDetail`;
 
   addToCart(product: Product) {
+    this.cartService.addToCart(product);
   }
 }
