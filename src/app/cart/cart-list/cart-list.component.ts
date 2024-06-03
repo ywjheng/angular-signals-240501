861b4b1027014ba41de3635da519e2d2f5bceec6
Ivy <ywjheng@gmail.com>
2024-06-03 15:58:13 +0800

    m11 - add Signal to Cart

--- src/app/cart/cart-list/cart-list.component.ts
+++ src/app/cart/cart-list/cart-list.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { NgFor, NgIf } from '@angular/common';
 import { CartItem } from '../cart';
 import { CartItemComponent } from '../cart-item/cart-item.component';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'sw-cart-list',
@@ -10,8 +11,7 @@ import { CartItemComponent } from '../cart-item/cart-item.component';
   templateUrl: 'cart-list.component.html'
 })
 export class CartListComponent {
-  // Just enough here for the template to compile
   pageTitle = 'Cart';
-
-  cartItems: CartItem[] = [];
+  private cartSercie = inject(CartService);
+  cartItems = this.cartSercie.cartItems;
 }
--- src/app/cart/cart.service.ts
+++ src/app/cart/cart.service.ts
@@ -1,8 +1,9 @@
-import { Injectable } from "@angular/core";
+import { Injectable, signal } from "@angular/core";
+import { CartItem } from "./cart";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
-
+  cartItems = signal<CartItem[]>([]);
 }
